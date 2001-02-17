da94415a5dfd57f6614726d18bd7330c235d144a
jah <empty>
2001-02-17 19:59:05 +0000

    games/NorthAndSouth/NorthAndSouth.asm: *** empty log message ***

--- games/NorthAndSouth/NorthAndSouth.asm
+++ games/NorthAndSouth/NorthAndSouth.asm
@@ -1,77 +1,127 @@
-;*---------------------------------------------------------------------------
+*---------------------------------------------------------------------------
 ;  :Program.	North&South.asm
 ;  :Contents.	Slave for "North & South" from Infogrames
-;  :Author.	Mr.Larmer of Wanted Team
-;  :Version.	$Id: North&South.asm 1.1 1998/09/06 10:28:50 jah Exp jah $
-;  :History.	20.12.98
+;  :Author.	Mr.Larmer of Wanted Team, Wepl
+;  :Original	
+;  :Version.	$Id: North&South.asm 1.0 2001/01/30 22:17:18 jah Exp $
+;  :History.	17.02.01 Wepl adjusted
 ;  :Requires.	-
 ;  :Copyright.	Public Domain
 ;  :Language.	68000 Assembler
-;  :Translator.	Devpac 3.14
+;  :Translator.	Asm-One 1.44
 ;  :To Do.
 ;---------------------------------------------------------------------------*
 
-	INCDIR	Include:
-	INCLUDE	lvo/exec_lib.i
-	INCLUDE	devices/trackdisk.i
+	INCDIR	Includes:
 	INCLUDE	whdload.i
-	OUTPUT	dh2:north&south/North&South.slave
-	OPT	O+ OG+			;enable optimizing
+	INCLUDE	whdmacros.i
 
-;======================================================================
+	IFD BARFLY
+	OUTPUT	"wart:n/north&south/North&South.Slave"
+	BOPT	O+	;enable optimizing
+	BOPT	OG+	;enable optimizing
+	BOPT	ODd-	;disable mul optimizing
+	BOPT	ODe-	;disable mul optimizing
+	BOPT	w4-	;disable 64k warnings
+	SUPER
+	ENDC
 
-base		SLAVE_HEADER		;ws_Security + ws_ID
-		dc.w	7		;ws_Version
-		dc.w	WHDLF_NoError|WHDLF_EmulTrap	;ws_flags
-		dc.l	$80000		;ws_BaseMemSize
-		dc.l	0		;ws_ExecInstall
-		dc.w	Start-base	;ws_GameLoader
-		dc.w	0		;ws_CurrentDir
-		dc.w	0		;ws_DontCache
-_keydebug	dc.b	$58		;ws_keydebug = F9
-_keyexit	dc.b	$59		;ws_keyexit = F10
+;============================================================================
 
-	dc.b	'$VER:North & South HD by Mr.Larmer/Wanted Team - V1.3 (20.12.98)',0
-	CNOP 0,2
+; number of floppy drives:
+;	sets the number of floppy drives, valid values are 0-4.
+;	0 means that the number is specified via option Custom1/N
+NUMDRIVES=1
 
-;======================================================================
-Start	;	A0 = resident loader
-;======================================================================
+; protection state for floppy disks:
+;	0 means 'write protected', 1 means 'read/write'
+;	bit 0 means drive DF0:, bit 3 means drive DF3:
+WPDRIVES=%0000
 
-		lea	_resload(pc),a1
-		move.l	a0,(a1)			;save for later use
+; disable fpu support:
+;	results in a different task switching routine, if fpu is enabled also
+;	the fpu status will be saved and restored.
+;	for better compatibility and performance the fpu should be disabled
+NOFPU
 
-		move.l	A0,A2
-		lea	OSEmu(pc),A0		;file name
-		lea	$400.w,A1		;address
-		jsr	resload_LoadFile(a2)
+; enable debug support for hrtmon:
+;	hrtmon reads to much from the stackframe if entered, if the ssp is at
+;	the end hrtmon will create a access fault.
+;	for better compatibility this option should be disabled
+;HRTMON
 
-		move.l	_resload(pc),a0
-		lea	base(pc),a1
-		jsr	$400.w
+; calculate minimal amount of free memory
+;	if the symbol MEMFREE is defined after each call to exec.AllocMem the
+;	size of the largest free memory chunk will be calculated and saved at
+;	the specified address if lower than the previous saved value (chipmem
+;	at MEMFREE, fastmem at MEMFREE+4)
+;MEMFREE=$100
 
-		move.w	#0,SR
+; amount of memory available for the system
+CHIPMEMSIZE	= $80000
+FASTMEMSIZE	= $0
 
-		jsr	_LVODoIO(a6)
-		move.l	IO_DATA(a1),a0
+;============================================================================
 
-; calculate checksum for bootblock (probably not run cracked version)
+KICKSIZE	= $40000			;34.005
+BASEMEM		= CHIPMEMSIZE
+EXPMEM		= KICKSIZE+FASTMEMSIZE
 
-		movem.l	D0-D2/A0-A2,-(a7)
+;============================================================================
 
-		lea	12(A0),A0
-		move.l	#$2A4,D0
-		move.l	_resload(pc),a2
-		jsr	resload_CRC16(a2)
+_base		SLAVE_HEADER			;ws_Security + ws_ID
+		dc.w	11			;ws_Version
+		dc.w	WHDLF_Disk|WHDLF_NoError|WHDLF_EmulTrap	;ws_flags
+		dc.l	BASEMEM			;ws_BaseMemSize
+		dc.l	0			;ws_ExecInstall
+		dc.w	_start-_base		;ws_GameLoader
+		dc.w	0			;ws_CurrentDir
+		dc.w	0			;ws_DontCache
+_keydebug	dc.b	0			;ws_keydebug
+_keyexit	dc.b	$59			;ws_keyexit = F10
+_expmem		dc.l	EXPMEM			;ws_ExpMem
+		dc.w	_name-_base		;ws_name
+		dc.w	_copy-_base		;ws_copy
+		dc.w	_info-_base		;ws_info
+
+;============================================================================
+
+_name		dc.b	"North & South",0
+_copy		dc.b	"1989 Infogrames",0
+_info		dc.b	"adapted by Wepl & Mr.Larmer",10
+		dc.b	"Version 1.4"
+		dc.b	0
+	EVEN
+
+;============================================================================
+_start	;	A0 = resident loader
+;============================================================================
+
+	;initialize kickstart and environment
+		bra	_boot
+
+	;a1 = ioreq ($2c+a5)
+	;a4 = buffer (1024 bytes)
+	;a6 = execbase
+_bootblock
+
+	;check version
+		move.l	#$2A4,d0
+		lea	12(a4),a0
+		move.l	(_resload,pc),a2
+		jsr	(resload_CRC16,a2)
 
 		cmp.w	#$8E19,D0
-		bne.b	.not_support
-
-		movem.l	(a7)+,D0-D2/A0-A2
-
+		beq	.verok
+.not_support
+		pea	TDREASON_WRONGVER
+		jmp	(resload_Abort,a2)
+.verok
 		pea	.patch(pc)
 
-		jmp	$C(a0)
+	;call bootblock
+		lea	($2c,a5),a1
+		jmp	(12,a4)
 .patch
 		addq.l	#8,A0
 
@@ -90,18 +140,11 @@ Start	;	A0 = resident loader
 		move.w	#$7001,$7F42(A0)		; skip protection
 
 		bra.b	.go
-.not_support
-		subq.l	#8,a7
-		pea	TDREASON_WRONGVER.w
-		move.l	_resload(pc),-(a7)
-		addq.l	#resload_Abort,(a7)
-		rts
 
-;--------------------------------
+;============================================================================
 
-_resload	dc.l	0		;address of resident loader
-OSEmu		dc.b	'OSEmu.400',0
+	INCLUDE	Sources:whdload/kick13.s
 
-;======================================================================
+;============================================================================
 
 	END
