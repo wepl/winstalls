ab37d12ccfef983650458b324d3f5026735b8bb3
Bert Jahn <wepl@whdload.de>
2024-08-18 22:51:07 +0200

    UnitA_FA18Interceptor: completed 1.1

--- ctros/UnitA_FA18Interceptor/unit-a.s
+++ ctros/UnitA_FA18Interceptor/unit-a.s
@@ -1,85 +1,91 @@
 ;*---------------------------------------------------------------------------
 ;  :Program.    Unit-A
 ;  :Contents.   "F/A 18 Interceptor" Crack-Intro (02/06/1988)
-;  :Author.     Stefano "Max Headroom/TEX" Pucino
+;  :Author.     Stefano "Max Headroom/TEX" Pucino, Wepl
 ;  :Requires.   Unit-A Intro (uncompressed, 57.844 bytes)
 ;  :Copyright.  Open Source (like GNU)
 ;  :Language.   68000 Assembler
 ;  :Translator. ASM-One v1.46 / Barfly v2.00
 ;  :To Do.
-;
-;  :History.
-;               03.10.2001 - Version 1.0
+;  :History.    03.10.2001 - Version 1.0
 ;                NEW: Removed SoundTracker-Bug
 ;                     Removed useless UNIT-check
+;		2024-08-18 Wepl:
+;		  changed from osemu to kickemu
+;		  added text message output
 ;---------------------------------------------------------------------------*
-
 ;======================================================================
 ; Load all includes and macros
 ;======================================================================
 
-    INCDIR  INCLUDE:
-    INCLUDE whdload.i
-    INCLUDE whdmacros.i
-    INCLUDE lvo/exec_lib.i
-    INCLUDE lvo/dos_lib.i
-
-;======================================================================
-; Special BarFly options and optimisations
-;======================================================================
-
-    IFD BARFLY
-        OUTPUT  "Unit-A.slave"              ; Name of the slave
-        BOPT    O+ OG+                      ; Enable optimizing
-        BOPT    ODd- ODe-                   ; Disable mul optimizing
-        BOPT    w4-                         ; Disable 64k warnings
-        SUPER                               ; Disable supervisor warnings
-    ENDC
+	INCLUDE whdload.i
+	INCLUDE	lvo/dos.i
 
 ;======================================================================
 
-_base   SLAVE_HEADER                        ; ws_Security + ws_ID
-        dc.w    14                          ; WHDLoad version needed
-        dc.w    WHDLF_NoError               ; Flags
-        dc.l    $80000                      ; BaseMem Size (512 KB)
-        dc.l    $0                          ; Exec Install
-        dc.w    _Start-_base                ; Gameloader
-        dc.w    0                           ; Current Dir
-        dc.w    0                           ; Don't Cache
-_keydebug
-        dc.b    $5f                         ; DebugKey = HELP
-_keyexit
-        dc.b    $5d                         ; Exit Key = PrtScr
-_expmem
-        dc.l    0                           ; ExpMem (No Fast-Mem)
-        dc.w    _name-_base                 ; Name of file
-        dc.w    _copy-_base                 ; Copyright
-        dc.w    _info-_base                 ; Additional informations
+CHIPMEMSIZE	= $80000	;size of chip memory
+FASTMEMSIZE	= $0		;size of fast memory
+NUMDRIVES	= 1		;amount of floppy drives to be configured
+WPDRIVES	= %0000		;write protection of floppy drives
+
+;BLACKSCREEN			;set all initial colors to black
+;BOOTBLOCK			;enable _bootblock routine
+BOOTDOS				;enable _bootdos routine
+;BOOTEARLY			;enable _bootearly routine
+;CBDOSLOADSEG			;enable _cb_dosLoadSeg routine
+;CBDOSREAD			;enable _cb_dosRead routine
+;CBKEYBOARD			;enable _cb_keyboard routine
+;CACHE				;enable inst/data cache for fast memory with MMU
+;CACHECHIP			;enable inst cache for chip/fast memory
+;CACHECHIPDATA			;enable inst/data cache for chip/fast memory
+;DEBUG				;add more internal checks
+;DISKSONBOOT			;insert disks in floppy drives
+;DOSASSIGN			;enable _dos_assign routine
+;FONTHEIGHT	= 8		;enable 80 chars per line
+HDINIT				;initialize filesystem handler
+;HRTMON				;add support for HrtMON
+;IOCACHE	= 1024		;cache for the filesystem handler (per fh)
+;MEMFREE	= $200		;location to store free memory counter
+;NEEDFPU			;set requirement for a fpu
+;POINTERTICKS	= 1		;set mouse speed
+;SEGTRACKER			;add segment tracker
+;SETKEYBOARD			;activate host keymap
+;SETPATCH			;enable patches from SetPatch 1.38
+;SNOOPFS			;trace filesystem handler
+;STACKSIZE	= 6000		;increase default stack
+;TRDCHANGEDISK			;enable _trd_changedisk routine
+;WHDCTRL			;add WHDCtrl resident command
+
+;============================================================================
+
+slv_Version	= 16
+slv_Flags	= WHDLF_NoError|WHDLF_Examine
+slv_keyexit	= $59		;F10
+
+;============================================================================
+
+	INCLUDE	whdload/kick13.s
 
 ;======================================================================
 ; The description part
 ; Strings are zero terminated. 10 means Carricage Return (CR)
 ;======================================================================
 
-_name
+slv_CurrentDir	dc.b	0
+slv_name
     dc.b    "F/A 18 Interceptor Crack-Intro",0  ; Full name of the intro
-_copy
+slv_copy
     dc.b    "1988 Unit-A",0                 ; Copyright information
-_info                                       ; Who am I ? ;)
+slv_info                                       ; Who am I ? ;)
     dc.b    "--------Installed by:--------",10
     dc.b    "Max Headroom",10
     dc.b    "of",10
     dc.b    "The Exterminators",10
+    db	    "Updated by Wepl",10
     dc.b    "-----------------------------",10
 
-    dc.b    "Version 1.0 "                  ; Installer-version
-    IFD      BARFLY
-        IFND    .passchk
-            DOSCMD  "WDate  >T:date"
-            INCBIN  "T:date"                ; Include current date of slave
-.passchk
-        ENDC
-    ENDC
+    dc.b    "Version 1.1 "                  ; Installer-version
+	INCBIN	.date
     dc.b    0                               ; End this string
     even
 
@@ -87,116 +93,50 @@ _info                                       ; Who am I ? ;)
 ; The magic part. Now we start the slave ;)
 ;======================================================================
 
-_Start                                      ; A0 = resident loader
+_bootdos
 
-; This routine simply loads the empty variable '_resload' to a1 and
-; puts the resident-loader (location=A0) to it.
+	;print text message
+		lea	$20000,a3
+		lea	_text,a0
+		move.l	a3,a1
+		move.l	_resload,a2
+		jsr	(resload_LoadFileDecrunch,a2)
+		move.l	d0,d3			;length
 
-    lea     _resload(pc),a1                 ; Get Slave-base
-    move.l  a0,(a1)                         ; Save for later use
-    move.l  a0,a2                           ; A2 = resload, too
-
-; Now we enable the Instruction-Cache but disable the Data-Cache at the
-; same time. This gives us a very huge compatibility.
-
-    move.l  #CACRF_EnableI,d0               ; Enable CPU Instruction-Cache
-    move.l  d0,d1                           ; Mask
-    jsr     (resload_SetCACR,a0)            ; WHD SetCache()
-
-;======================================================================
-; Clear Memory
-;======================================================================
-
-; This isn't important, so you can also leave this out. But for
-; a nice and real clean memory we will "simulate" a software reset
-; by clearing the memory from $10000 to $80000.
-
-    lea.l   $10000,a0                       ; Get Start into a0
-    move.l  #$6fffff,d0                     ; How many bytes to clear ?
-.clearloop
-    clr.l   (a0)+                           ; Clear a0 ($10000)
-    dbra    d0,.clearloop                   ; Continue until d0 is -1
+		lea	(_dosname,pc),a1
+		move.l	(4),a6
+		jsr	(_LVOOldOpenLibrary,a6)
+		move.l	d0,a6			;A6 = dosbase
+		jsr	(_LVOOutput,a6)
+		move.l	d0,d1
+		move.l	a3,d2
+		jsr	(_LVOWrite,a6)
 
 ;======================================================================
-; Load & initialize the OSEmu module
-;======================================================================
-
-; Without this we wouldn't be able to use the DOS-function LoadSeg()
-; to directly load and relocate an executable for our needs.
-
-    lea     _OSEmu(pc),a0                   ; file name
-    lea     $400.w,a1                       ; A3 = OSEmu base address
-    jsr     (resload_LoadFileDecrunch,a2)   ; Load and decrunch OSEmu file
-
-; Initialize OSEmu
-
-    move.l  a2,a0                           ; resload
-    lea     _base(pc),a1                    ; slave structure
-    jsr     $400.w                          ; Start it
-
-    move.w  #0,sr                           ; switch to user mode
-
-; Open dos.library to provide file loading function
-
-    moveq.l #0,d0                           ; Clear d0 (=lib-version not important)
-    lea     _dosname(pc),a1                 ; load dos.library
-
-    move.l  $4.w,a6                         ; Get execbase to a6
-    jsr     _LVOOpenLibrary(a6)             ; Open the library
-    lea.l   _dospointer(pc),a4              ; Load dos-pointer address to a4
-    move.l  d0,(a4)                         ; Put dos-pointer to a4
-    move.l  d0,a6                           ; Put it also to a6
-
-;======================================================================
-; Load the intro via dos.library LoadSeg() function
+; Load the intro
 ;======================================================================
 
     lea     _exe(pc),A0                     ; Get executable-filename
-    move.l  a0,d1                           ; And copy it to d0 also
-    jsr     _LVOLoadSeg(A6)                 ; Load the file now.
+	move.l	a3,a1
+	jsr	(resload_LoadFileDecrunch,a2)
+	move.l	a3,a0
+	sub.l	a1,a1
+	jsr	(resload_Relocate,a2)
 
-    lsl.l   #2,d0                           ; Left-scroll result by 2
-    move.l  d0,a1                           ; Copy result to a1
-    addq.l  #4,a1                           ; Add 4 to it
+	lea	_pl,a0
+	move.l	a3,a1
+	movea.l	(_resload,pc),a2
+	jsr	(resload_Patch,a2)
 
-    sub.l   a0,a0                           ; Clear a0 now
-    moveq.l #0,d0                           ; No pointer on argumentline
+	jsr	(a3)
 
-    lea     _introstart(pc),a2              ; Get variable
-    move.l  a1,(a2)                         ; Save start-address of game
-
-;======================================================================
-; The now loaded and relocated executable will be fixed here.
-;======================================================================
-
-; We will fix some routines in the intro before we start the show.
-
-    jsr     _fix(pc)                        ; Jump to the fixing-routine
-
-    move.l  _introstart(pc),a1              ; Source-Address
-    lea.l   _arg(pc),a0                     ; Argument line
-    moveq.l #1,d0                           ; One argument
-    jsr     (a1)                            ; Start the decruncher
-    jmp     _exitintro(pc)                  ; Jump to EXIT-OF-INTRO
-
-_fix
-    move.l  _introstart(pc),a1              ; Source-Address
-    move.w  #$4eb9,$8f6(a1)                 ; Poke JSR to destination
-    pea     (_dbra,pc)                      ; Put SoundTracker-fix on Stack
-    move.l  (a7)+,$8f8(a1)                  ; Write Stack to destination
-
-    move.w  #$4e71,$8fc(a1)                 ; Add 2 NOP-words after the call
-    move.w  #$4e71,$8fe(a1)                 ; to overwrite old command
-
-    move.w  #$4e71,$420(a1)                 ; Remove call to "UNIT"-test
-    move.w  #$4e71,$422(a1)                 ; and infinite loop
-    rts
+	; wait for releasing RMB
+.wait	btst	#10-8,(_custom+potinp)
+	beq	.wait
 
 ;======================================================================
 ; Exit slave
 ;======================================================================
-
-_exitintro
 
 ; We put the "OK" reason to the stack and load the resident-base to a0.
 ; Then we abort the whole show giving WHD the reason.
@@ -208,6 +148,11 @@ _exitintro
 ;======================================================================
 ; FIX Routines
 ;======================================================================
+
+_pl	PL_START
+	PL_PSS	$8f6,_dbra,4
+	PL_S	$420,4				; Remove call to "UNIT"-test and infinite loop
+	PL_END
 
 ;======================================================================
 ; We have to fix an empty DBRA-loop at $2e27e !
@@ -260,12 +205,6 @@ _dbra:
 ; Additional data and variables
 ;======================================================================
 
-_OSEmu      dc.b    'OSEmu.400',0           ; Name of the OSEmu module to emulate DOS functions
 _exe        dc.b    "Unit-A",0              ; Name of the Intro
-    even
-_resload    dc.l    0                       ; Address of resident loader
-_dospointer dc.l    0                       ; DOS.library base pointer
-_introstart dc.l    0                       ; Start-address after LoadSeg()
-_arg        dc.b    $a                      ; Argument-line (CR=Empty)
-_dosname    dc.b    'dos.library',0         ; DOS.library name
-    even
+_text		db	"rage",0		; text message file
+
