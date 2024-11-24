eab4766f31ac57800f438c4c2f19fe92280e43bb
Bert Jahn <wepl@whdload.de>
2024-11-24 22:33:47 +0100

    Cybersphere/Plus: final 1.0

---
+++ games/Cybersphere/cybersphere.asm
@@ -0,0 +1,160 @@
+;*---------------------------------------------------------------------------
+;  :Modul.	cybersphere.asm
+;  :Contents.	Cybersphere
+;  :Author.	Wepl
+;  :History.	2024-11-20 started
+;  :Requires.	kick13.s
+;  :Copyright.	Public Domain
+;  :Language.	68000 Assembler
+;  :To Do.
+;---------------------------------------------------------------------------*
+
+	INCLUDE	whdload.i
+	INCLUDE	whdmacros.i
+	INCLUDE	lvo/dos.i
+
+;============================================================================
+
+CHIPMEMSIZE	= $70000	;size of chip memory
+FASTMEMSIZE	= $60000	;size of fast memory
+NUMDRIVES	= 1		;amount of floppy drives to be configured
+WPDRIVES	= %0000		;write protection of floppy drives
+
+BLACKSCREEN			;set all initial colors to black
+;BOOTBLOCK			;enable _bootblock routine
+BOOTDOS				;enable _bootdos routine
+;BOOTEARLY			;enable _bootearly routine
+;CBDOSLOADSEG			;enable _cb_dosLoadSeg routine
+;CBDOSREAD			;enable _cb_dosRead routine
+;CBKEYBOARD			;enable _cb_keyboard routine
+;CACHE				;enable inst/data cache for fast memory with MMU
+;CACHECHIP			;enable inst cache for chip/fast memory
+;CACHECHIPDATA			;enable inst/data cache for chip/fast memory
+;DEBUG				;add more internal checks
+;DISKSONBOOT			;insert disks in floppy drives
+;DOSASSIGN			;enable _dos_assign routine
+;FONTHEIGHT	= 8		;enable 80 chars per line
+HDINIT				;initialize filesystem handler
+;HRTMON				;add support for HrtMON
+IOCACHE		= 1024		;cache for the filesystem handler (per fh)
+;MEMFREE	= $200		;location to store free memory counter
+;NEEDFPU			;set requirement for a fpu
+;POINTERTICKS	= 1		;set mouse speed
+;SEGTRACKER			;add segment tracker
+;SETKEYBOARD			;activate host keymap
+;SETPATCH			;enable patches from SetPatch 1.38
+;SNOOPFS			;trace filesystem handler
+;STACKSIZE	= 6000		;increase default stack
+;TRDCHANGEDISK			;enable _trd_changedisk routine
+;WHDCTRL			;add WHDCtrl resident command
+
+;============================================================================
+
+slv_Version	= 16
+slv_Flags	= WHDLF_NoError|WHDLF_Examine|WHDLF_NoKbd
+slv_keyexit	= $59	;F10
+
+;============================================================================
+
+	INCLUDE	whdload/kick13.s
+
+;============================================================================
+
+slv_CurrentDir	dc.b	"data",0
+slv_name	dc.b	"Cybersphere",0
+slv_copy	dc.b	"1994 Psycon",0
+slv_info	dc.b	"adapted for WHDLoad by Wepl",10
+		dc.b	"Version 1.0 "
+		INCBIN	".date"
+		dc.b	0
+	IFGE slv_Version-17
+slv_config	dc.b	"C1:B:Trainer",0
+	ENDC
+_program	dc.b	"cybersphere",0
+	EVEN
+
+;============================================================================
+; like a program from "startup-sequence" executed, full dos process,
+; HDINIT is required, this will never called if booted from a diskimage, only
+; works in conjunction with the virtual filesystem of HDINIT
+; this routine replaces the loading and executing of the startup-sequence
+
+	IFD BOOTDOS
+
+_bootdos
+		move.l	(_resload,pc),a2	;A2 = resload
+
+	;open doslib
+		lea	(_dosname,pc),a1
+		move.l	(4),a6
+		jsr	(_LVOOldOpenLibrary,a6)
+		move.l	d0,a6			;A6 = dosbase
+
+	;check version
+		lea	(_program,pc),a0	;name
+		move.l	#300,d3			;maybe 300 byte aren't enough for version compare...
+		move.l	d3,d0			;length
+		moveq	#0,d1			;offset
+		sub.l	d3,a7
+		move.l	a7,a1			;buffer
+		jsr	(resload_LoadFileOffset,a2)
+		move.l	d3,d0
+		move.l	a7,a0
+		jsr	(resload_CRC16,a2)
+		add.l	d3,a7
+
+		cmp.w	#$c2e1,d0
+		beq	.versionok
+		pea	TDREASON_WRONGVER
+		jmp	(resload_Abort,a2)
+.versionok
+
+	;load exe
+		lea	(_program,pc),a0
+		move.l	a0,d1
+		jsr	(_LVOLoadSeg,a6)
+		move.l	d0,d7			;D7 = segment
+		beq	.program_err
+
+	;patch
+		lea	(_pl_program,pc),a0
+		move.l	d7,a1
+		jsr	(resload_PatchSeg,a2)
+
+	IFD DEBUG
+	;set debug
+		clr.l	-(a7)
+		move.l	d7,-(a7)
+		pea	WHDLTAG_DBGSEG_SET
+		move.l	a7,a0
+		jsr	(resload_Control,a2)
+		add.w	#12,a7
+	ENDC
+
+	;call
+		move.l	d7,a0
+		add.l	a0,a0
+		add.l	a0,a0
+		jsr	(4,a0)
+	
+	;quit
+		pea	TDREASON_OK
+		move.l	(_resload,pc),a2
+		jmp	(resload_Abort,a2)
+
+.program_err	jsr	(_LVOIoErr,a6)
+		pea	(_program,pc)
+		move.l	d0,-(a7)
+		pea	TDREASON_DOSREAD
+		jmp	(resload_Abort,a2)
+
+_pl_program	PL_START
+		PL_R	$9a		;getting vbr
+		PL_END
+
+	ENDC
+
+;============================================================================
+
+	END
+
