33952d42ed672dba0c5015ef0725e52c1307714b
Bert Jahn <wepl@whdload.de>
2025-12-18 16:53:55 +0100

    EmeraldMine2: fix MANX stack check

--- games/EmeraldMine2/emeraldmine2.asm
+++ games/EmeraldMine2/emeraldmine2.asm
@@ -1,8 +1,11 @@
 ;*---------------------------------------------------------------------------
 ;  :Program.	emeraldmine2.asm
 ;  :Contents.	Slave for "Emerald Mine 2"
-;  :Author.	Harry
+;  :Author.	Harry, Wepl
 ;  :History.	09.06.2012 started
+;		18.12.2025 imported to winstalls
+;			   add Config to start editor
+;			   fix MANX stack check
 ;  :Requires.	-
 ;  :Copyright.	Public Domain
 ;  :Language.	68000 Assembler
@@ -19,93 +22,69 @@
 ;1 $7904 $4fc
 ;etc.
 
-
-
-		INCDIR	"asm-one:include/"
-		INCLUDE	whdload/whdload.i
-		INCLUDE	whdload/whdmacros.i
-		INCLUDE	dos/dos_lib.i
-		INCLUDE	exec/exec.i
-		INCLUDE exec/exec_lib.i
-		INCLUDE	libraries/expansion_lib.i
-
-	IFNE	0
-	INCDIR	Includes:
 	INCLUDE	whdload.i
 	INCLUDE	whdmacros.i
-	INCLUDE	lvo/dos.i
-
-	IFD BARFLY
-	OUTPUT	"wart:li/lordsofwar/LordsOfWar.Slave"
-	BOPT	O+				;enable optimizing
-	BOPT	OG+				;enable optimizing
-	BOPT	ODd-				;disable mul optimizing
-	BOPT	ODe-				;disable mul optimizing
-	BOPT	w4-				;disable 64k warnings
-	SUPER
-	ENDC
-	ENDC
 
 ;============================================================================
 
 CHIPMEMSIZE	= $E0000
 FASTMEMSIZE	= $0000
-NUMDRIVES	= 2
+NUMDRIVES	= 1
 WPDRIVES	= %0000
 
-;BLACKSCREEN
-;BOOTBLOCK
-BOOTDOS
-;BOOTEARLY
-;CBDOSLOADSEG
-;CBDOSREAD
-;CACHE
-DEBUG
-;DISKSONBOOT
-;DOSASSIGN
-;FONTHEIGHT	= 8
-HDINIT
-;HRTMON
-IOCACHE		= 1024
-;MEMFREE        = $100
-;NEEDFPU
-;POINTERTICKS	= 1
-;SETPATCH
-;STACKSIZE	= 6000
-;TRDCHANGEDISK
-
+;BLACKSCREEN			;set all initial colors to black
+;BOOTBLOCK			;enable _bootblock routine
+BOOTDOS				;enable _bootdos routine
+;BOOTEARLY			;enable _bootearly routine
+;CBDOSLOADSEG			;enable _cb_dosLoadSeg routine
+;CBDOSREAD			;enable _cb_dosRead routine
+;CBKEYBOARD			;enable _cb_keyboard routine
+;CACHE				;enable inst/data cache for fast memory with MMU
+;CACHECHIP			;enable inst cache for chip/fast memory
+;CACHECHIPDATA			;enable inst/data cache for chip/fast memory
+DEBUG				;add more internal checks
+;DISKSONBOOT			;insert disks in floppy drives
+;DOSASSIGN			;enable _dos_assign routine
+;FONTHEIGHT	= 8		;enable 80 chars per line
+HDINIT				;initialize filesystem handler
+;HRTMON				;add support for HrtMON
+IOCACHE	= 4096			;cache for the filesystem handler (per fh)
+;MEMFREE	= $100		;location to store free memory counter
+;NEEDFPU			;set requirement for a fpu
+;POINTERTICKS	= 1		;set mouse speed
+SEGTRACKER			;add segment tracker
+;SETKEYBOARD			;activate host keymap
+;SETPATCH			;enable patches from SetPatch 1.38
+;SNOOPFS			;trace filesystem handler
+;STACKSIZE	= 6000		;increase default stack
+;TRDCHANGEDISK			;enable _trd_changedisk routine
+;WHDCTRL			;add WHDCtrl resident command
 
 ;============================================================================
 
-slv_Version	= 16
+slv_Version	= 20
 slv_Flags	= WHDLF_NoError|WHDLF_Examine
 slv_keyexit	= $59	;F10
 
 ;============================================================================
 
-;	INCLUDE	Sources:whdload/kick13.s
-	INCLUDE	kick13.s
+	INCLUDE	whdload/kick13.s
 
 ;============================================================================
-
-	IFD BARFLY
-	DOSCMD	"WDate  >T:date"
-	ENDC
 
 slv_CurrentDir	dc.b	"data",0
 slv_name	dc.b	"Emerald Mine 2",0
 slv_copy	dc.b	"1988 Kingsoft",0
-slv_info	dc.b	"adapted by Harry",10
-		dc.b	"Version 0.91 "
-;	IFD BARFLY
-;		INCBIN	"T:date"
-;	ENDC
+slv_info	dc.b	"adapted by Harry, Wepl",10
+		dc.b	"Version 1.0 "
+		INCBIN	.date
 		dc.b	0
+slv_config	dc.b	"C1:B:Start Editor",0
+slv_MemConfig	= slv_base
 _program	dc.b	"em2",0
 _program2	dc.b	"newdef",0
 _args		dc.b	10
-_args_end
-		dc.b	0
+_args_end	dc.b	0
 	EVEN
 _prognamtab	dc.w	0,_program2-_program
 
@@ -123,8 +102,7 @@ _prognamtab	dc.w	0,_program2-_program
 ; (4,SP)     stack size
 ; (8,SP)     previous stack frame -> +4 = A1,A2,A5,A6
 
-
-_bootdos        move.l  (_resload,pc),a2        ;A2 = resload
+_bootdos	move.l	(_resload,pc),a2	;A2 = resload
 
 	lea	_whtags(pc),a0
 	jsr	(resload_Control,a2)
@@ -167,8 +145,6 @@ _bootdos        move.l  (_resload,pc),a2        ;A2 = resload
 		jsr	(resload_CRC16,a2)
 		add.l	d3,a7
 
-;	illegal
-
 		cmp.w	#$5368,d0
 		beq	.versionok
 		
@@ -199,7 +175,6 @@ _bootdos        move.l  (_resload,pc),a2        ;A2 = resload
 
 .versionok
 
-
 	;load exe
 		lea	(_program,pc),a0
 		moveq.l	#0,d0
@@ -210,9 +185,6 @@ _bootdos        move.l  (_resload,pc),a2        ;A2 = resload
 		jsr	(_LVOLoadSeg,a6)
 		move.l	d0,d7			;D7 = segment
 		beq	.program_err
-		move.l	d7,d0
-		lsl.l	#2,d0
-		move.l	d0,$F0.W	;start of exe just for my debugger
 
 		move.b	_editoractive(pc),d0
 		beq.s	.gameem2
@@ -228,17 +200,11 @@ _bootdos        move.l  (_resload,pc),a2        ;A2 = resload
 	move.l	(a7)+,-$1e+2(a6)
 	bra.s	.callprg
 
-
-
-;	illegal
-
-
 .gameem2	move.b	#2,$80.W	;no of drives: 2 (both disks inserted)
 
 		move.w	#$4ef9,$88.w
 		lea	_loopdbf(pc),a0
 		move.l	a0,$8a.w
-
 
 	;patch
 		lea	(_pl_program,pc),a0
@@ -260,25 +226,15 @@ _bootdos        move.l  (_resload,pc),a2        ;A2 = resload
 ;	move.l	a1,$a6(a0)
 ;	endc
 
-
-
-	IFD DEBUG1
-	;set debug
-		clr.l	-(a7)
-		move.l	d7,-(a7)
-		pea	WHDLTAG_DBGSEG_SET
-		move.l	a7,a0
-		jsr	(resload_Control,a2)
-		add.w	#12,a7
-	ENDC
-
 	;call
 .callprg
-		move.l	d7,d1
 		moveq	#_args_end-_args,d0
 		lea	(_args,pc),a0
-		bsr	.call
-;	illegal
+		move.l	d7,d1
+		lsl.l	#2,d1
+		move.l	d1,a3
+		addq.l	#4,a7			;drop os return address to fix MANX stack check
+		jsr	(4,a3)
 		pea	TDREASON_OK
 		move.l	(_resload,pc),a2
 		jmp	(resload_Abort,a2)
@@ -289,15 +245,6 @@ _bootdos        move.l  (_resload,pc),a2        ;A2 = resload
 		pea	TDREASON_DOSREAD
 		jmp	(resload_Abort,a2)
 
-; D0 = ULONG arg length
-; D1 = BPTR  segment
-; A0 = CPTR  arg string
-
-.call		lsl.l	#2,d1
-		move.l	d1,a3
-		jmp	(4,a3)
-
-	IFEQ	0
 _pl_program	PL_START
 		PL_PS	$8a8,_remdiskaccess
 		PL_P	$25a,_loopbne
@@ -344,7 +291,6 @@ _pl_program	PL_START
 .corrstoneshiftrightend
 
 		PL_END
-	ENDC
 
 ; < d1 seglist
 ; < d2 section #
@@ -454,9 +400,7 @@ _random	lea	RANDOM1(PC),A0
 RANDOM1	DC.L	$3F3F751F
 RANDOM2	DC.L	$17179834
 
-
 ;============================================================================
-
 
 _editoractive
 	dc.b	0
